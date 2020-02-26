bd5e51e3b5004abd70af696d839b87b49c225dd4
FusRoman <roman.lemontagner@gmail.com>
2020-02-26 16:52:54 +0100

    add camera to canvas (zqsd to move it

--- script/main.js
+++ script/main.js
@@ -10,12 +10,93 @@ let main_menu = new Menu(background_context, 0, 0, background_canvas.clientWidth
     main_menu.drawMenu();
 }*/
 
-aabb1 = new AABB(10,10,100,100);
-aabb2 = new AABB(50,50,70,50);
-circle1 = new Circle(100,30,50);
-circle2 = new Circle(500,300,50);
+aabb1 = new AABB(10, 10, 100, 100);
+aabb2 = new AABB(50, 50, 70, 50);
+aabb3 = new AABB(-50, 0, 50, 50);
+circle1 = new Circle(100, 30, 50);
+circle2 = new Circle(500, 300, 50);
 
 aabb1.drawAABBCollideBox(background_context);
 aabb2.drawAABBCollideBox(background_context);
+aabb3.drawAABBCollideBox(background_context);
 circle1.drawCircleCollideBox(background_context);
 circle2.drawCircleCollideBox(background_context);
+
+let toggleCamera = false;
+let buttonCameraPressed = new Set();
+/*
+function camera(){
+
+    if(toggleCamera){
+        switch(keyCodeCamera){
+            case "90":
+                background_context.translate(0, -10);
+                break;
+        }
+    }
+
+}*/
+
+let lastLoop = performance.now();
+let fps = 0;
+background_context.font = "30px Arial";
+
+function fpsCounter() {
+    delta = (performance.now() - lastLoop) / 1000;
+    lastLoop = performance.now();
+    fps = Math.round(1 / delta);
+    return fps;
+}
+
+let camera = new Vector2D(0, 0);
+
+function continuousCamera(context) {
+    if (buttonCameraPressed.has(90)) {
+        context.translate(0, -5);
+        camera.y -= 5;
+    }
+    if (buttonCameraPressed.has(81)) {
+        context.translate(-5, 0);
+        camera.x -= 5;
+    }
+    if (buttonCameraPressed.has(83)) {
+        context.translate(0, 5);
+        camera.y += 5;
+    }
+    if (buttonCameraPressed.has(68)) {
+        context.translate(5, 0);
+        camera.x += 5;
+    }
+}
+
+function gameStep(timeStep) {
+    background_context.clearRect(0 - camera.x, 0 - camera.y, background_canvas.width, background_canvas.height);
+
+    continuousCamera(background_context);
+    background_context.fillText(fpsCounter(), 10 - camera.x, 30 - camera.y);
+    aabb1.drawAABBCollideBox(background_context);
+    aabb2.drawAABBCollideBox(background_context);
+    aabb3.drawAABBCollideBox(background_context);
+    circle1.drawCircleCollideBox(background_context);
+    circle2.drawCircleCollideBox(background_context);
+
+
+    requestAnimationFrame(gameStep);
+}
+
+requestAnimationFrame(gameStep);
+
+
+document.addEventListener("keydown", function (event) {
+    if (!toggleCamera) {
+        toggleCamera = true;
+    }
+    buttonCameraPressed.add(event.keyCode);
+});
+
+document.addEventListener("keyup", function (event) {
+    if (buttonCameraPressed.size == 0) {
+        toggleCamera = false;
+    }
+    buttonCameraPressed.delete(event.keyCode);
+});
