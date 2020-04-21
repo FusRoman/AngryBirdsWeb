3d02058fa0487d150f8dad08d22865e4c8add007
Matthieu <matthieu.robeyns@u-psud.fr>
2020-04-21 19:39:11 +0200

    Bug a fixer sur le Retry

--- public/script/gameLogic/gameLogic.js
+++ public/script/gameLogic/gameLogic.js
@@ -1,10 +1,12 @@
 class GameLogic {
 
-    constructor(canvas, context, menu) {
+    constructor(canvas, context, menu,loose,win) {
         this.context = context;
         this.context.font = "20px serif";
         this.canvas = canvas;
         this.menu = menu;
+        this.loose = loose;
+        this.win = win;
 
         this.hGround = 500;
         let groundShape = new Rectangle(-1000, this.hGround, 6000, 100);
@@ -53,10 +55,15 @@ class GameLogic {
 
             if (this.nbTarget == 0 && this.isInGame) {
                 this.score += 5 * this.nbBall;
-                this.leftGame();
+                //this.leftGame();
+                this.isInGame = false;
+                this.youWon(this.win);            
             }
-            if (this.nbShoot == 0 && this.nbTarget != 0 && this.isInGame) {
-                this.leftGame();
+            if (this.nbBall == 0 && this.nbTarget != 0 && this.isInGame) {
+
+                //this.leftGame();
+                this.isInGame = false;
+                this.youLoose(this.loose,this.actualIdLevel);                
             }
             this.cannon.draw(this.context);
 
@@ -81,6 +88,8 @@ class GameLogic {
         this.score = 0;
         this.canvas.style.display = "initial";
         this.menu.style.display = "none";
+        this.loose.style.display = "none";
+        this.win.style.display = "none";
     }
 
     leftGame() {
@@ -196,12 +205,118 @@ class GameLogic {
         });
     }
 
-    youLoose() {
-        return this.score;
+    youLoose(loose,level) {
+        let mySelf=this;
+        loose.setAttribute("style", "display: initial; position: absolute; top: 320px; left: 360px");
+        this.context.strokeStyle = "#000000";
+        this.context.fillStyle = "#000000";
+        this.context.fillText("Tu as Perdu!", 400 - this.camera.coordCamera.x  , 240 - this.camera.coordCamera.y);
+        this.context.fillText("Ton score : " + this.score, 400 - this.camera.coordCamera.x, 280 - this.camera.coordCamera.y);
+        
+        //Création d'un boutton
+        let menuButton = document.createElement("button");
+        menuButton.setAttribute("id", "BacktoMenu");
+        menuButton.setAttribute("style", "2pt solid black; display: initial");
+        menuButton.innerHTML = "Back to Menu";
+
+        let retryButton = document.createElement("button");
+        retryButton.setAttribute("id", "Retry");
+        retryButton.setAttribute("style", "2pt solid black; display: initial");
+        retryButton.innerHTML = "Retry";
+        
+        //Pas fini donc a ne pas prendre en compte.
+        menuButton.onclick = function(){
+            game_logic.initLevelMenu();
+        };
+
+        //Ici l'action du boutton
+        retryButton.onclick = function (){
+            let remove = document.getElementById("Retry");
+            let remove2 = document.getElementById("BacktoMenu");
+            loose.removeChild(remove);
+            loose.removeChild(remove2);            
+            mySelf.leftGame();
+            mySelf.enterLevel();
+            let path = "level/level" + level.toString() + "/desc.json";
+            loadFromServer(path).then((value) => {
+                let levelDesc = JSON.parse(value);
+                mySelf.nbBall = levelDesc["nbBall"];
+                let wall = levelDesc["wall"];
+                let target = levelDesc["target"];
+                mySelf.actualIdLevel = levelDesc["idlevel"];
+                let id = 3;
+                mySelf.nbTarget = target.length;
+                target.forEach(target => {
+                    let newTarget = mySelf.createTarget(target, id);
+                    mySelf.gameObject.push(newTarget);
+                    ++id;
+                });
+                wall.forEach((wall) => {
+                    let newWall = mySelf.createWall(wall, id);
+                    mySelf.gameObject.push(newWall);
+                    ++id;
+                });
+            });
+              
+            
+        };
+        loose.append(menuButton);
+        loose.append(retryButton);
     }
 
-    youWon() {
-        return this.score;
+    youWon(win) {
+        win.setAttribute("style", "display: initial" );
+        win.setAttribute("position", "absolute" );    
+        this.context.strokeStyle = "#000000";
+        this.context.fillStyle = "#000000";
+        this.context.fillText("Tu as Gagné!", 400 - this.camera.coordCamera.x, 240 - this.camera.coordCamera.y);
+        this.context.fillText("Ton score : " + this.score, 400 - this.camera.coordCamera.x, 280 - this.camera.coordCamera.y);
+
+        let menuButton = document.createElement("button");
+        menuButton.setAttribute("id", "BacktoMenu");
+        menuButton.setAttribute("style", "2pt solid black; display: initial");
+        menuButton.innerHTML = "Back to Menu";
+
+        let nextButton = document.createElement("button");
+        nextButton.setAttribute("id", "NextLevel");
+        nextButton.setAttribute("style", "2pt solid black; display: initial");
+        nextButton.innerHTML = "Next Level";
+        
+        let niveau = this.actualIdLevel+1;
+        let changeMenuButton = document.getElementById(niveau.toString());
+        changeMenuButton.disabled = !specificLevelDesc["resolue"];
+
+        menuButton.onclick = function(){
+            game_logic.initLevelMenu();
+        };
+
+        nextButton.onclick = function (){            
+            let mySelf = this;
+            mySelf.enterLevel();
+            let levelName = "level"+niveau.toString();
+            let path = "level/" + levelName + "/desc.json";
+            loadFromServer(path).then((value) => {
+                let levelDesc = JSON.parse(value);
+                mySelf.nbBall = levelDesc["nbBall"];
+                let wall = levelDesc["wall"];
+                let target = levelDesc["target"];
+                mySelf.actualIdLevel = levelDesc["idlevel"];
+                let id = 3;
+                mySelf.nbTarget = target.length;
+                target.forEach(target => {
+                    let newTarget = mySelf.createTarget(target, id);
+                    mySelf.gameObject.push(newTarget);
+                    ++id;
+                });
+                wall.forEach((wall) => {
+                    let newWall = mySelf.createWall(wall, id);
+                    mySelf.gameObject.push(newWall);
+                    ++id;
+                });
+            });
+        };
+        win.appendChild(menuButton);
+        win.appendChild(nextButton);
     }
 
     /*
--- public/script/main.js
+++ public/script/main.js
@@ -1,9 +1,11 @@
 let canvas = document.getElementById("background_canvas");
 let context = canvas.getContext("2d");
 let menu = document.getElementById("menu");
+let loose = document.getElementById("Loose");
+let win = document.getElementById("Win");
 
 
-let game_logic = new GameLogic(canvas, context, menu);
+let game_logic = new GameLogic(canvas, context, menu,loose,win);
 
 window.onload = function(){
     game_logic.initLevelMenu();
